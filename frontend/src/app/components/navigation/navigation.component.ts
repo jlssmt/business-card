d4069d5ad94c54f73309d23ab196857097250547
jls <jls.smt@gmail.com>
2020-11-13 12:46:17 +0100

    restyled side-nav

--- frontend/src/app/components/navigation/navigation.component.ts
+++ frontend/src/app/components/navigation/navigation.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
 
@@ -16,6 +16,8 @@ export class NavigationComponent {
       shareReplay()
     );
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(private breakpointObserver: BreakpointObserver) {
+
+  }
 
 }
